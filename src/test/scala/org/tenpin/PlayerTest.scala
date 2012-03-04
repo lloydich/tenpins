c8eedad2b0c5b9d2d3b0e44309517c576d4a2d0f
Lloyd Lane <lloydich@gmail.com>
2012-03-04 22:51:48 +0000

    revamp where frame is an object 2

--- src/test/scala/org/tenpin/PlayerTest.scala
+++ src/test/scala/org/tenpin/PlayerTest.scala
@@ -7,7 +7,36 @@ class PlayerTest extends FunSuite {
     val player = new Player()
     def printStuff {
       println(player.score())
-      println(player.ballScores)
+      println(player.ballScores)                                                                                                                                                                                                                                                         package org.tenpin
+
+      import org.scalatest.FunSuite
+
+      class PlayerTest extends FunSuite {
+        test("score result should have matched expected") {
+          val player = new Player()
+          def printStuff {
+            println(player.score())
+            println(player.ballScores)
+            println(player.frameScores)
+          }
+          player.add(1)
+          printStuff
+          assert(player.score == 0)
+
+          player.add(2)
+          printStuff
+          assert(player.score == 3)
+
+          player.add(5)
+          printStuff
+          assert(player.score == 3)
+
+      //    player.add(4)
+      //    printStuff
+      //    assert(player.score == 12)
+        }
+      }
+
       println(player.frameScores)
     }
     player.add(1)
