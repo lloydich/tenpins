b9afbb4b7b9233ac73d50da67e73edf81b6826be
Lloyd Lane <lloydich@gmail.com>
2012-03-06 22:43:22 +0000

    cleaned up Player add() to have no side effects

--- src/main/scala/org/tenpin/Player.scala
+++ src/main/scala/org/tenpin/Player.scala
@@ -1,49 +1,37 @@
 package org.tenpin
 
-class Player {
-               //to be moved to player object inside list
- var frameScores:List[Int]   = List[Int]()
-var ballScores:List[List[Int]]   = List[List[Int]]()
+import grizzled.slf4j.Logging
 
-  
-  def score():Int={
-    if(frameScores.isEmpty) 0 else frameScores.reduceLeft((a:Int,  b:Int) => a + b )
+class Player extends Logging {
+
+  var frameScores: List[Int] = List[Int]()
+  var ballScores: List[List[Int]] = List[List[Int]]()
+
+
+  def score(): Int = {
+    if (frameScores.isEmpty) 0 else frameScores.reduceLeft((a: Int, b: Int) => a + b)
   }
-  
-  def add(ballScore:Int){
-    println("\nadding "+ballScore)
-               //get size of ballscores
-    //if not 0 get last and initialise frame see if its done
-    //if not done add to frame otherwise create new array and add
-    //if frame finished add to frameScore
-    def getCurrentFrame:List[Int] = {
-      println("getCurrentFrame  ")
+
+  def add(ballScore: Int) {
+    info("\nadding " + ballScore)
+
+    def createNewBallScores: List[List[Int]] = {
       if (ballScores.isEmpty) {
-        println("getCurrentFrame  if")
-        ballScores=ballScores:::List(List(ballScore))
-        List(ballScore)
+        ballScores ::: List(List(ballScore))
       }
       else {
-
-        var currentFrame = ballScores.last
-        println("getCurrentFrame else currentFrame:"+currentFrame)
-        if (!Frame.isFrameInPlay(currentFrame)){
-          println("getCurrentFrame else if - frame is NOT in play")
-          ballScores=ballScores:::List(List(ballScore))
-            List(ballScore)
+        if (!Frame.isFrameInPlay(ballScores.last)) {
+          ballScores ::: List(List(ballScore))
         }
         else {
-          println("getCurrentFrame else else - frame IS in play")
-           currentFrame = List(ballScore):::currentFrame
-          println("currentFrame updated to:"+currentFrame)
-          ballScores=ballScores.dropRight(1)
-          ballScores = ballScores:::List(currentFrame)
-          currentFrame
+          ballScores.dropRight(1) ::: List(ballScores.last ::: List(ballScore))
         }
       }
     }
-    var currentFrame:List[Int]  = getCurrentFrame
-    if (!Frame.isFrameInPlay(currentFrame))
-      frameScores = frameScores:::List(Frame.getCurrentScore(currentFrame))
+
+    ballScores = createNewBallScores
+
+    if (!Frame.isFrameInPlay(ballScores.last))
+      frameScores = frameScores ::: List(Frame.getCurrentScore(ballScores.last))
   }
 }
